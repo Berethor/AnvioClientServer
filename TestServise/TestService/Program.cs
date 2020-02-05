ebea3b177efc15f55f470c1a2910b65d6e7b0fa8
Вова <blizzardVovan4ek@gmail.com>
2020-02-05 14:48:45 +0300

    Создал сервис запускающий сервер.

---
+++ TestServise/TestService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace TestService
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            var serv = new Service();
+            serv.OnDebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#else
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
---
+++ TestServise/TestService/Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ServiceProcess;
+using TestServer;
+
+namespace TestService
+{
+    public partial class Service : ServiceBase
+    {
+        private Server server;
+        public Service()
+        {
+            InitializeComponent();
+        }
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+        protected override void OnStart(string[] args)
+        {
+            server = new Server(6777);
+            server.Start();
+        }
+
+        protected override void OnStop()
+        {
+            server.Stop();
+        }
+    }
+}
