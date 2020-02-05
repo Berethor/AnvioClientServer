36fc4b6535960a180ff780e2aedf9a14ed982e59
Вова <blizzardVovan4ek@gmail.com>
2020-02-05 16:29:38 +0300

    Рефакторинг кода. Изменил инструкцию по установке службы.

--- TestServise/TestService/ProjectInstaller.cs
+++ TestServise/TestService/ProjectInstaller.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration.Install;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace TestService
 {
     [RunInstaller(true)]
-    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    public partial class ProjectInstaller : Installer
     {
         public ProjectInstaller()
         {
